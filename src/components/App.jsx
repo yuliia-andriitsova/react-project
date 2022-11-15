a122d6df6418b9ebac12d7e816dc5d9ffa8b0774
yuliia-andriitsova <andriitsovayulia@gmail.com>
2022-11-15 22:59:12 +0200

    styles-added modal-close

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Header from './Header/Header';
+import Modal from './Modal/Modal';
 
 export const App = () => {
   return (
     <>
+      <Modal />
+
       <Routes>
         <Route path="/" element={<Header />} />
       </Routes>
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { createPortal } from 'react-dom';
 import css from './Modal.module.css';
+const modalWindow = document.querySelector('#modal-root');
 
 export default function Modal() {
-  return (
+  return createPortal(
     <div className={css.Overlay}>
       <div className={css.Modal}>
+        <button type="button" className={css.ModalCloseIcon}>
+          <svg
+            width="12"
+            height="12"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M11.8333 1.3415L10.6583 0.166504L5.99996 4.82484L1.34163 0.166504L0.166626 1.3415L4.82496 5.99984L0.166626 10.6582L1.34163 11.8332L5.99996 7.17484L10.6583 11.8332L11.8333 10.6582L7.17496 5.99984L11.8333 1.3415Z"
+              fill="black"
+            />
+          </svg>
+        </button>
         <h2 className={css.ModalTitle}>
           Your recommended daily calorie intake is
         </h2>
@@ -13,8 +29,10 @@ export default function Modal() {
           <span className={css.ModalCalories}> kcal</span>
         </p>
         <h3>Foods you should not eat</h3>
+        <span className={css.ModalLine}></span>
         <ol className={css.ModalListNotEat}></ol>
       </div>
-    </div>
+    </div>,
+    modalWindow
   );
 }
