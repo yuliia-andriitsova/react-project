f612e43ff1d26aeef01d551474041f4ae6e2ae4a
Nadiia23 <108667064+Nadiia23@users.noreply.github.com>
2022-11-15 19:41:15 +0200

    Update RegistrationForm

---
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -0,0 +1,41 @@
+import { useDispatch } from 'react-redux';
+
+
+const RegistrationForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    dispatch(
+      register({
+        username: form.elements.name.value,
+        email: form.elements.email.value,
+        password: form.elements.password.value,
+      })
+    );
+    form.reset();
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name
+          <input type="text" name="name" required/>
+        </label>
+        <label>
+          Email
+          <input type="email" name="email" required/>
+        </label>
+        <label>
+          Password
+          <input type="password" name="password" required/>
+        </label>
+      </form>
+    </div>
+  );
+};
+
+export default RegistrationForm;
