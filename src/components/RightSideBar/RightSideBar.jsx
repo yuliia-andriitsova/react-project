d54c3625280665e71731a921894d727cca5a0523
Nadiia23 <108667064+Nadiia23@users.noreply.github.com>
2022-11-19 15:13:28 +0200

    comit

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -1,7 +1,7 @@
 import moment from 'moment';
 import React from 'react';
 import { useSelector } from 'react-redux';
-import css from './RightSideBar.module.css';
+import scss from './RightSideBar.module.scss';
 import { selectDaySummary } from 'redux/Product-search/product-search-selectors';
 import { selectnotAllowedProducts } from 'redux/User/user-selectors';
 
@@ -10,37 +10,45 @@ export default function RightSideBar() {
   const notAllowedProducts = useSelector(selectnotAllowedProducts);
 
   return (
-    <div>
-      <h3>Summary for {moment(daySummary.date).format('DD/MM/YYYY')}</h3>
-      <table>
-        <tbody className={css.RightSideBarTable}>
-          <tr>
-            <td>Left</td>
-            <td>{daySummary.kcalLeft} kcal</td>
-          </tr>
-          <tr>
-            <td>Consumed</td>
-            <td>{daySummary.kcalConsumed} kcal</td>
-          </tr>
-          <tr>
-            <td>Daily rate</td>
-            <td> {daySummary.dailyRate} kcal</td>
-          </tr>
-          <tr>
-            <td>n% of normal</td>
-            <td>{daySummary.percentsOfDailyRate} kcal</td>
-          </tr>
-        </tbody>
-      </table>
-      <h3 className={css.RightSideBarTitle}>Food not recommended</h3>
-      <span className={css.RightSideBarText}>
-        Your diet will be displayed here
-      </span>
-      <ol className={css.ModalListNotEat}>
-        {notAllowedProducts.slice(0, 10).map(product => (
-          <li key={product}>{product}</li>
-        ))}
-      </ol>
+    <div className={scss.container}>
+      <div className={scss.rightSideBarWrap}>
+        <h3 className={scss.rightSideBarTitle}>
+          Summary for {moment(daySummary.date).format('DD/MM/YYYY')}
+        </h3>
+        <ul className={scss.rightSideBarList}>
+          <li className={scss.rightSideBarItem}>
+            <span className={scss.itemText}>Left</span>
+            <span className={scss.itemText}>{daySummary.kcalLeft} kcal</span>
+          </li>
+          <li className={scss.rightSideBarItem}>
+            <span className={scss.itemText}>Consumed</span>
+            <span className={scss.itemText}>
+              {daySummary.kcalConsumed} kcal
+            </span>
+          </li>
+          <li className={scss.rightSideBarItem}>
+            <span className={scss.itemText}>Daily rate</span>
+            <span className={scss.itemText}> {daySummary.dailyRate} kcal</span>
+          </li>
+          <li className={scss.rightSideBarItem}>
+            <span className={scss.itemText}>n% of normal</span>
+            <span className={scss.itemText}>
+              {daySummary.percentsOfDailyRate} kcal
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3 className={scss.rightSideBarTitle}>Food not recommended</h3>
+        <span className={scss.itemText}>Your diet will be displayed here</span>
+        <ul className={scss.notAllowedProductsList}>
+          {notAllowedProducts.slice(0, 10).map(product => (
+            <li className={scss.itemText} key={product}>
+              {product},
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
