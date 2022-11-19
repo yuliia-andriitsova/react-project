af2a13b3a96b613422bdab678795817989fe0077
Polina Igrushkina <polina.igrushkina@gmail.com>
2022-11-20 00:28:40 +0300

    Started LoginPage

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,10 @@
 import LoginForm from 'components/LoginForm/LoginForm';
 import Logo from 'components/Logo/Logo';
+import scss from './LoginPage.module.scss';
 
 export default function LoginPage() {
   return (
-    <div>
+    <div className={scss.container}>
       <Logo />
       <LoginForm />
     </div>
