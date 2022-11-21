9683b5f24ceede225a19da841dadb4af9298c17b
ivantalko <108145743+ivantalko@users.noreply.github.com>
2022-11-21 17:17:16 +0200

    super

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,22 +5,27 @@ import MainPage from 'pages/MainPage/MainPage';
 import RegistrationPage from 'pages/RegistrationPage/RegistrationPage';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { getUserOperation } from 'redux/User/user-operation';
-import PrivateRoute from './PrivateRoute/PrivateRoute';
-import PublicRoute from './PublicRoute/PublicRoute';
-import scss from './App/App.module.scss';
-import Header from './Header/Header';
+import PrivateRoute from '../PrivateRoute/PrivateRoute';
+import PublicRoute from '../PublicRoute/PublicRoute';
+import scss from './App.module.scss';
+import Header from '../Header/Header';
 
 export const App = () => {
+  const location = useLocation();
+  console.log(location);
   const dispatch = useDispatch();
-
+  const isMainbackground =
+    location.pathname === '/' ||
+    location.pathname === '/signin' ||
+    location.pathname === '/registration';
   useEffect(() => {
     dispatch(getUserOperation());
   }, [dispatch]);
 
   return (
-    <div className={scss.backgraundImg}>
+    <div className={isMainbackground ? scss.backgraundImg : scss.secondaryBcg}>
       <Header />
       <Routes>
         <Route path="/" element={<PublicRoute />}>
