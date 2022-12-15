f55389f6d5c8b07f988a07d545ebb19cfcc9536d
yuliia-andriitsova <andriitsovayulia@gmail.com>
2022-12-15 17:31:56 +0200

    final

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="react-team-project">
+        <BrowserRouter basename="react-project">
           <App />
         </BrowserRouter>
       </PersistGate>
